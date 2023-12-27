0d8c3fd45290fc81cc6d167981b3b740aba0ed08
PumkinB <olipumknb@gmail.com>
2023-12-27 10:59:53 +0700

    perubahan di js

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 
 
-const form = document.getElementById("myForm");
-const error = [];
+const form = document.getElementById('form');
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault()
-
-    var username = document.getElementById("username");
-    var tanggal = document.getElementById("tanggal");
-    var success = ""
-
-    if(username.value == ""){
-        error.push("Please isi nama dulu");
-    }
-    
-    if(tanggal.value == ""){
-        error.push("isi tanggal tempat lahir dulu");
-    }
-    alert("Form is submitted");
-
-    var message = document.getElementById ("message");
-    message.innerText = error
-
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 });
 
+const nameInput = document.getElementById('username');
+const dateInput = document.getElementById('tanggal');
+const messageInput = document.getElementById('pesan');
+
+function kirimForm () {
+    const name = nameInput.value;
+    const date = dateInput.value;
+    const sexInput = document.querySelector('input[name ="sex"]:checked');
+    const sex = sexInput ? sexInput.value : '';
+    const message = messageInput.value;
+
+    if (name.length < 3){
+        alert('Masukkan nama');
+        return;
+    }
+
+    if (!date){
+        alert('Masukkan tanggal lahir');
+        return;
+    }
+
+    if (!sex) {
+        alert('Pilih jenis kelamin');
+        return;
+    }
+
+    if (message.length < 10){
+        alert('Masukkan pesan minimal 10 karakter');
+        return;
+    }
+
+    alert('TERIMA KASIH');
+    location.reload();
+
+}
